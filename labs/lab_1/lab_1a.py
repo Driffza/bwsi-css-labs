e0f510e2a008df6c6c6a5317761f96632a8920a1
Driffza <griffindesouza@gmail.com>
2026-06-17 19:49:13 -0500

    Update lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,6 @@ def main():
     name = "Griffin DeSouza" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
-
+    print("Hope you enjoy!")
 if __name__ == "__main__":
     main()
