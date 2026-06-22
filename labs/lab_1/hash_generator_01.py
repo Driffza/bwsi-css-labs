83fbee771769d2701d3c2459f4826370472729a8
Driffza <griffindesouza@gmail.com>
2026-06-22 18:40:47 -0500

    Create hash_generator_01.py

---
+++ labs/lab_1/hash_generator_01.py
@@ -0,0 +1,17 @@
+import numpy as np
+import hashlib
+
+def my_hash(data):
+    if not isinstance(data, str):
+        d2 = data.tobytes()
+    else:
+        d2 = data.encode('utf-8')
+       
+    return hashlib.md5(d2).hexdigest()
+
+def round_and_report(data, num_decimals = 3):
+    d2 = np.rint(data * 10**num_decimals).astype(np.int32)
+    return my_hash(d2)
+arr = np.array([np.pi, np.pi*3, 9.93493])
+
+print((round_and_report(arr)))
